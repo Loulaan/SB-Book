2702800781d0cb1a3652a8e856e3dc0abb9e9db1
Анастасия Мищенко <anastasia@MacBook-Pro-Anastasia.local>
2020-03-21 18:12:26 +0300

    main page layout mvp

--- angular-front/src/app/app.module.ts
+++ angular-front/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { TextFieldComponent } from './text-field/text-field.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-    TextFieldComponent
+    TextFieldComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
